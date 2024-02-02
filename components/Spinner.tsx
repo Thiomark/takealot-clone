0e6553beeec44d5bcf9ff798238e935cfc6bc7c1
Itumeleng Tsoela <it@vectra.co.za>
2024-02-02 14:43:21 +0200

    feat: added the loading spinner

---
+++ components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Spinner = () => {
+  return <div className="loader"></div>;
+};
+
+export default Spinner;
--- components/single-product/AddToCartButtonsComponent.jsx
+++ components/single-product/AddToCartButtonsComponent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useProducts } from "@/providers/ProductProvider";
 import { useCart } from "@/providers/CartProvider";
+import Spinner from "@/components/Spinner";
 
 const AddToCartButtonsComponent = ({ extraStyle }) => {
   const { product } = useProducts();
-  const { addItemToCart, addItemToList } = useCart();
+  const { addItemToCart, addItemToList, loading: cartLoading } = useCart();
   const productToAddTocart = {
     id: product.id,
     qty: 1,
@@ -22,15 +23,21 @@ const AddToCartButtonsComponent = ({ extraStyle }) => {
         onClick={() => addItemToCart(productToAddTocart)}
         className="flex text-sm font-bold items-center justify-center space-x-2 text-white bg-green-450 w-full p-3"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          className="bi h-5 w-5 bi-cart"
-          viewBox="0 0 16 16"
-        >
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </svg>
-        <span>Add to Cart</span>
+        {cartLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="bi h-5 w-5 bi-cart"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </svg>
+            <span>Add to Cart</span>
+          </>
+        )}
       </button>
       <button
         onClick={() => addItemToList(product)}
